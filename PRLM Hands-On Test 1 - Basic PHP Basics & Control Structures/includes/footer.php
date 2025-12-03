37ef7cdcf5e0d50dcdd4b87a2d6dd102f8805f4d
reve-bit <jnlarioza.cscsoc@gmail.com>
2025-12-03 14:57:19 +0800

    Add files via upload

---
+++ PRLM Hands-On Test 1 - Basic PHP Basics & Control Structures/includes/footer.php
@@ -0,0 +1,3 @@
+    <footer>&copy; <?php echo date('Y')?>  Justine Lee Larioza</footer>
+  </body>
+</html>
---
+++ PRLM Hands-On Test 1 - Basic PHP Basics & Control Structures/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Reve Store</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+  <body>
+    <h1>The Reve Store</h1>
+    <nav>
+      <a href="Test1.php">Home</a> | 
+      <a href="Test2.php">RevePhone</a> | 
+      <a href="about.php">About</a> | 
+      <a href="contact.php">Contact</a>
+    </nav>
